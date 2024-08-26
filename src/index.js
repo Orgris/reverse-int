f3a7bb35b819de29ec652b8e12cd11f469737aa7
Orgris <vjmax793@gmail.com>
2024-08-26 17:08:15 +0300

    feat: add reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let num = n;
+    let reversNum = "";
+    let numString = num.toString();
+
+    console.log(numString);
+
+    for (let i = numString.length - 1; i >= 0; i--) {
+        if (numString[i] !== '-'){
+            reversNum += numString[i];
+        }
+    }
+    console.log(reversNum);
+
+    return reversNum;
+};
